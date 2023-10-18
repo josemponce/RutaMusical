5bc7ac2a936049a6e6187553901b4323dff9af76
danieeeld2 <danieeeld2@gmail.com>
2023-10-18 18:53:36 +0200

    Avanza josemponce/RutaMusical#8
    Closes josemponce/RutaMusical#9
    
    Avanza josemponce/RutaMusical#10
    
    Closes josemponce/RutaMusical#11
    
    Closes josemponce/RutaMusical#14
    
    Creacion de los conciertos

---
+++ internal/conciertos.go
@@ -0,0 +1,13 @@
+package internal
+
+import "time"
+
+type Concierto struct {
+	Fecha 			time.Time
+	HoraInicio 		time.Time
+	NombrePueblo 	string
+	DineroPagado 	float64
+	Duracion 		int // minutos
+	Latitud 		float64
+	Longitud 		float64
+}
