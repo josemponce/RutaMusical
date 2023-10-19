6dfc5c578f6e409b473b965f4e50e498a1580ce4
danieeeld2 <danieeeld2@gmail.com>
2023-10-19 09:22:24 +0200

    Avanza josemponce/RutaMusical#10
    Closes josemponce/RutaMusical#13
    
    Tras lo reflexionado en estos issues, no necesitamos estos parametros en la agrupacion

--- internal/agrupacion.go
+++ internal/agrupacion.go
@@ -3,8 +3,4 @@ package internal
 type AgrupacionMusical struct {
 	NombreAgrupacion 		string
 	Musicos 			[]Musico
-	
-	// Coordenadas pueblo de la agrupacion
-	Latitud 			float64
-	Longitud 			float64
 }
